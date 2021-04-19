d389cb2f3166a526bf5880a2e32c06be7fc946dd
Gregor Karetka <karetka.gregor@gmail.com>
2021-04-19 18:52:27 +0200

    Merge (lock controller and hex driver and it's stuff)

---
+++ door_lock_system/door_lock_system.srcs/sources_1/new/lock_controller.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/19/2021 02:46:47 PM
+-- Design Name: 
+-- Module Name: lock_controller - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lock_controller is
+    Port (
+        
+    );
+end lock_controller;
+
+architecture Behavioral of lock_controller is
+
+begin
+
+
+end Behavioral;
